e34fdb2cbf7b6b7e454e0883a09ffc28d77b1a36
CMax00 <max.loktev.52@googlemail.com>
2026-04-01 21:23:25 +0200

    grid with buttons

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import kivy
 kivy.require('2.3.1')
 
 from kivy.app import App
-from kivy.uix.label import Label
-
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        layout = GridLayout(cols=4, rows=4, spacing=10, padding=20)
+
+        for i in range(15):
+            btn = Button(text=str(i+ 1), font_size=32)
+            layout.add_widget(btn)
+
+        return layout
 
 
 if __name__ == '__main__':
