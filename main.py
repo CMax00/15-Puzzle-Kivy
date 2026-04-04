5b59c0bf520e51524bd628c63f994021f925115d
CMax00 <max.loktev.52@googlemail.com>
2026-04-04 23:13:49 +0200

    add visual Grid randomizer

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 import kivy
+import random
 
 kivy.require('2.3.1')
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.clock import Clock
 
 blocks = []
 empty = (0, 0)
+oldEmpty = (-10, -10)
 gridSize = 4
+mixSteps = 500
+timeBetweenMixSteps = 0
 layout = GridLayout(cols=gridSize, rows=gridSize, spacing=10, padding=20)
+
+
+def mixGrid():
+    x, y = 0, 0
+    # Generates random move until it is valid
+    while (x + y == 0  # prevents no movement
+           or empty[0] + x < 0 or empty[0] + x > gridSize - 1  # prevents impossible move
+           or empty[1] + y < 0 or empty[1] + y > gridSize - 1
+           or oldEmpty == (empty[0] + x, empty[1] + y)):  # prevents undoing last move
+        # Makes a random move
+        x = random.randrange(-1, 2)
+        y = random.randrange(-1, 2)
+
+    # Finds the selected block to move
+    for block in blocks:
+        if (block.x, block.y) == (empty[0] + x, empty[1] + y):
+            block.move(None)
+            break
+
+
+def mixStep(dt):
+    global mixSteps
+    # Stops when no steps remain
+    if mixSteps <= 0:
+        return False
+
+    mixGrid()
+    mixSteps -= 1
 
 
 # puts the blocks into the grid according to their pos
@@ -39,6 +72,7 @@ class Block:
     # switches block with empty if it is next to it
     def move(self, instance):
         global empty
+        global oldEmpty
 
         for x in range(-1, 2):
             for y in range(-1, 2):
@@ -46,6 +80,7 @@ class Block:
                 if abs(x) + abs(y) != 2:
                     # tests if that po is empty
                     if self.x + x == empty[0] and self.y + y == empty[1]:
+                        oldEmpty = empty
                         empty = (self.x, self.y)
                         self.x, self.y = self.x + x, self.y + y
 
@@ -83,6 +118,11 @@ class MyApp(App):
                 blocks.append(block)
 
         return layout
+
+    def on_start(self):
+        # Run the mixSteps
+        # This method is used so the screen updates between the Steps
+        Clock.schedule_interval(mixStep, timeBetweenMixSteps)
 
 
 if __name__ == '__main__':
